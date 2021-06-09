76c605ac919815225baaab99c774fa0d649b0105
Hrishikesh Kulkarni <hmkulkarni2001@gmail.com>
2021-06-09 19:27:41 +0530

    Alert added on contact

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let con = document.getElementById("myContactForm");
+con.addEventListener("submit", (e) => {
+  e.preventDefault();
+  Swal.fire({
+    title: "Thank you for contacting!",
+    text: "We will reach out to you as soon as possible.",
+    icon: "success",
+    button: "Close",
+  });
+});
